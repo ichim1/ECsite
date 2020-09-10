2ce40e307b4032906841e46dc19e5384aeed03c7
ichim1 <snkichimbsns@gmail.com>
2020-09-10 11:15:15 +0900

    initial commit

---
+++ ecsite/src/main/java/jp/co/internous/ecsite/model/entity/Goods.java
@@ -0,0 +1,60 @@
+package jp.co.internous.ecsite.model.entity;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="goods")
+public class Goods {
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name="goods_name")
+	private String goodName;
+	
+	@Column(name="price")
+	private long price;
+	
+	@Column(name="updated_at")
+	private Timestamp updateAt;
+	
+	public long getId() {
+		return id;
+	}
+	
+	public void setId(long id) {
+		this.id=id;
+	}
+	
+	public String getGoodName() {
+		return goodName;
+	}
+	
+	public void setGoodName(String goodName) {
+		this.goodName=goodName;
+	}
+	public long getPrice() {
+		return price;
+	}
+	
+	public void setPrice(long price) {
+		this.price=price;
+	}
+	public Timestamp getUpdateAt() {
+		return updateAt;
+	}
+	
+	public void setUpdateAt(Timestamp updateAt) {
+		this.updateAt=updateAt;
+	}
+
+}
